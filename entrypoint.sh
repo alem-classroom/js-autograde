8c90469ac34f64d5362342c12b8688a34887edd7
Zulbukharov <l1pt0n1905@gmail.com>
2020-07-21 15:06:54 +0600

    feat: new domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,7 +53,7 @@ z=$(find $TEST -mindepth 1 -maxdepth 1 -type d -name "test*" -print0 | xargs -n 
 
 send_result(){
     # apikey user lesson done 
-    curl -s -X POST "https://learn.alem.school/api/service/grade" -H "x-grade-secret: ${1}" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\":\"${2}\", \"lesson\":\"${3}\", \"status\": \"${4}\"}" > /dev/null
+    curl -s -X POST "https://lrn.dev/api/service/grade" -H "x-grade-secret: ${1}" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\":\"${2}\", \"lesson\":\"${3}\", \"status\": \"${4}\"}" > /dev/null
 }
 
 for LESSON_NAME in $z
